0df0bebe375dbdce6c636f8ef2554118457562d3
dohee <doheeeq@gmail.com>
2025-09-08 10:53:44 +0900

    ref 전달로 수량 끌어올리기

--- src/components/Meals/MealItem.jsx
+++ src/components/Meals/MealItem.jsx
@@ -1,15 +1,17 @@
 import styles from './MealItem.module.scss';
 import MealItemForm from './MealItemForm';
-import {useContext} from "react";
-import CartContext from "../context/cart-context.js";
 
 const MealItem = ({ id, price, description, name }) => {
-    const x = useContext(CartContext);
-    console.log('mealItem x: ', x)
 
     const { meal, description: desc, price: priceStyle } = styles;
 
     const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
+
+    // cart.jsx 에게 보낼 데이터를 생성하는 함수
+    // 선택한 수량 끌어올리기
+    const handleAddToCart = (amount) => {
+        console.log('선택한 수량: ', amount)
+    }
 
     return (
     <li className={meal}>
@@ -19,7 +21,7 @@ const MealItem = ({ id, price, description, name }) => {
         <div className={priceStyle}>{formatPrice}원</div>
       </div>
       <div>
-        <MealItemForm id={id} />
+        <MealItemForm id={id} onAddToCart={handleAddToCart}/>
       </div>
     </li>
     );
--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -1,16 +1,31 @@
 import Input from '../UI/Input';
 import styles from './MealItemForm.module.scss';
+import {useRef} from "react";
 
-const MealItemForm = ({la}) => {
+const MealItemForm = ({id, onAddToCart}) => {
+
+    // ref 전달
+    const inputRef = useRef();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        onAddToCart(inputRef.current.value);
+    }
+
   return (
-    <form className={styles.form}>
-      <Input
-        label='수량'
-        x = {{
-            type: 'text',
-            onFocus: () => console.log("focus!")
-        }}
-      />
+    <form className={styles.form} onSubmit={handleSubmit}>
+        <Input
+            label='수량'
+            inputAttr={{
+                id: 'amount_' + id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }}
+            ref={inputRef}
+        />
       <button>담기</button>
     </form>
   );
