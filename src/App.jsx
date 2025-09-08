180f2064530bc0f5318a5044323c46845173282f
dohee <doheeeq@gmail.com>
2025-09-08 09:42:32 +0900

    카드 모달 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Header from "./components/Layout/Header";
 import './App.css';
 import Meals from "./components/Meals/Meals";
+import {useState} from "react";
+import Cart from "./components/Cart/Cart.jsx";
 
 const App = () => {
+
+    // 장바구니 모달을 여닫는 상태변수
+    const[cartIsShown, setIseCartIsShown] =useState(false);
+
+    // 모달을 열어주는 함수
+    const handelShowCart = () => setIseCartIsShown(true);
+    const handleHideCart = () =>  setIseCartIsShown(false);
+
     return <>
-        <Header />
+        { cartIsShown && <Cart onClose = {handleHideCart} />}
+        <Header  onShowCart={handelShowCart}/>
         <div id="main">
             <Meals />
         </div>
--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -1,19 +1,15 @@
 import Input from '../UI/Input';
 import styles from './MealItemForm.module.scss';
 
-const MealItemForm = (props) => {
+const MealItemForm = ({la}) => {
   return (
     <form className={styles.form}>
       <Input
         label='수량'
-        // input={{
-        //   id: 'amount_' + props.id,
-        //   type: 'number',
-        //   min: '1',
-        //   max: '5',
-        //   step: '1',
-        //   defaultValue: '1',
-        // }}
+        x = {{
+            type: 'text',
+            onFocus: () => console.log("focus!")
+        }}
       />
       <button>담기</button>
     </form>
