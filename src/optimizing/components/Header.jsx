02612985ca12c2afa523d7296bd9536074fd26b8
dohee <doheeeq@gmail.com>
2025-09-09 09:48:22 +0900

    useCallback으로 함수 ㅈ재생성 방지

---
+++ src/optimizing/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <header id='main-header'>
+      <h1>React Optimizing</h1>
+    </header>
+  );
+};
+// export default React.memo(Header);
+export default Header;
