8b678e9d1d0efcef0409efd90214a014727bab22
dohee <doheeeq@gmail.com>
2025-09-09 10:51:52 +0900

    zustand 기초 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
-
-import Counter from './optimizing/components/Counter/Counter';
-import Header from './optimizing/components/Header';
-import { log } from './optimizing/log';
-import './App.css';
-import ConfigureCounter from "./optimizing/components/Counter/ConfigureCounter.jsx";
+import React from 'react';
+import ZustandCounter from "./zustand-practice/components/ZustandCounter.jsx";
 
 const App = () => {
-    log('<App /> rendered');
-
-    const [chosenCount, setChosenCount] = useState(0);
-
-    // 끌어올리기용 함수
-    const setCounterHandler = (number) => {
-        setChosenCount(number)
-    }
     return (
         <>
-            <Header />
-            <main>
-                <ConfigureCounter onSet={setCounterHandler}/>
-                <Counter key={chosenCount} /* 강제로 키를 변경시켜서 리렌더링 되게 함. 리액트는 키가 변경되면 상태를 원래대로 되돌림 */ initialCount={chosenCount} />
-            </main>
+            <ZustandCounter />
         </>
     );
 };
