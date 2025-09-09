6e2806102b0642fa473f5828662cb5b54f0eeb43
dohee <doheeeq@gmail.com>
2025-09-09 09:48:18 +0900

    useCallback으로 함수 ㅈ재생성 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import React, { useState } from 'react';
+
+import Counter from './optimizing/components/Counter/Counter';
+import Header from './optimizing/components/Header';
+import { log } from './optimizing/log';
 import './App.css';
-import CartProvider from "./components/context/CartProvider.jsx";
-import MainCart from "./components/Layout/MainCart.jsx";
+import ConfigureCounter from "./optimizing/components/Counter/ConfigureCounter.jsx";
 
 const App = () => {
+    log('<App /> rendered');
 
-    return <>
-        {/* value 속성에 하위 컴포넌트들이 공유할 상태값들을 명시 */}
-        <CartProvider>  {/* 제공자는 즉시 소비 못함, 하위 컴포넌트에서부터 소비할 수 있음, 제공자 = provider, 소비자: consumer*/}
-            <MainCart />
-        </CartProvider>
-    </>;
+    const [chosenCount, setChosenCount] = useState(0);
+
+    // 끌어올리기용 함수
+    const setCounterHandler = (number) => {
+        setChosenCount(number)
+    }
+    return (
+        <>
+            <Header />
+            <main>
+                <ConfigureCounter onSet={setCounterHandler}/>
+                <Counter initialCount={chosenCount} />
+            </main>
+        </>
+    );
 };
 
 export default App;
